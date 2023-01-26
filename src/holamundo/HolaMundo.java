89a13ba481e470660432f96f213c98e2f6c908bd
agustincho7 <ioriiiori7@gmail.com>
2023-01-26 15:13:35 -0300

    HolaMundo

---
+++ src/holamundo/HolaMundo.java
@@ -0,0 +1,15 @@
+package holamundo;
+
+public class HolaMundo {
+	
+	public static void main(String[] args) {
+		String nombre = "Agustin";
+		String apellido = "Avella";
+		int edad = 0;
+				
+		System.out.println("Hola Mundo desde Java!");
+		System.out.println(apellido);
+		
+	}
+
+}
