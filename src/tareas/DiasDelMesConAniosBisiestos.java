81b5d9ae46d9a893bebc26609e195fb5c5974076
AgustinAvella <aavella@americavirtualsa.com>
2023-02-17 03:10:15 -0300

    Agrego cases

--- src/tareas/DiasDelMesConAniosBisiestos.java
+++ src/tareas/DiasDelMesConAniosBisiestos.java
@@ -10,7 +10,7 @@ public class DiasDelMesConAniosBisiestos {
 		
 		switch (meses) {
 		case 1:
-			System.out.println("Este mes es enero y tiene x dias!");
+			System.out.println("Este mes es enero y tiene 31 dias!");
 			break;
 		case 2:
 			if (anios%4 == 0 && anios%100 != 0 || anios%400 == 0) {
@@ -20,24 +20,34 @@ public class DiasDelMesConAniosBisiestos {
 			}
 			break;
 		case 3:
+			System.out.println("Este mes es marzo y tiene 31 dias!");
 			break;
 		case 4:
+			System.out.println("Este mes es abril y tiene 30 dias!");
 			break;
 		case 5:
+			System.out.println("Este mes es mayo y tiene 31 dias!");
 			break;
 		case 6:
+			System.out.println("Este mes es junio y tiene 30 dias!");
 			break;
 		case 7:
+			System.out.println("Este mes es julio y tiene 31 dias!");
 			break;
 		case 8:
+			System.out.println("Este mes es agosto y tiene 31 dias!");
 			break;
 		case 9:
+			System.out.println("Este mes es septiembre y tiene 30 dias!");
 			break;
 		case 10:
+			System.out.println("Este mes es octubre y tiene 31 dias!");
 			break;
 		case 11:
+			System.out.println("Este mes es noviembre y tiene 30 dias!");
 			break;
 		case 12:
+			System.out.println("Este mes es diciembre y tiene 31 dias!");
 			break;
 		}
 		
